29a62ee6438c9b01f9f4e7a8d38312f469160347
YNikitin <n989yv@gmail.com>
2020-01-25 20:06:12 +0300

    add assert and rename test

--- test_items.py
+++ test_items.py
@@ -3,6 +3,7 @@ import pytest
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_add_to_basket_button_exist(browser):
     browser.get(link)
-    browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
+    add_button = browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert add_button != None
